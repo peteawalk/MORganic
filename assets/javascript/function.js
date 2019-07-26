5f2fc0cbbb226238d6f322b669436cfd21b67f8c
Zeus <reeseborchersmsu@gmail.com>
2019-07-25 19:10:00 -0500

    load

--- assets/javascript/function.js
+++ assets/javascript/function.js
@@ -3,21 +3,20 @@ function displayanimalInfo() {
 
     //var city = $(this).attr("data-animal");
     var queryURL =
-        "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=Kb4RvBmaZ97LxOtipmzdPpj1OZcnJGTRrlu3EOY5&location=Denver+CO"
+        "https://organicapi.ams.usda.gov/IntegrityPubDataServices/OidPublicDataService.svc/soap?api_key=fs8eDIZ5dYaS0SAqmwVyZEpIIgwqhm0FSofqHzSH"
 
 
     $.ajax({
         url: queryURL,
-        method: "POST"
+        method: "GET"
     }).then(function (response) {
 
-        var results = response.Operations;
+        var results = response.data;
 
         console.log(results);
 
     });
 
-
 }
 
 
@@ -25,41 +24,3 @@ function displayanimalInfo() {
 
 
 
-
-
-
-
-
-var map;
-var infoWindow;
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 38.899, //KU Edwards Campus
-            lng: -94.725
-        },
-        zoom: 6
-    });
-    infoWindow = new google.maps.InfoWindow;
-
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-        }, function () {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-}
