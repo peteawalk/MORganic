cd011b248052fd2c94311226ee3a00686aba03e0
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2019-07-26 22:32:57 -0500

    updated all function.js https requests to https

--- assets/javascript/function.js
+++ assets/javascript/function.js
@@ -29,7 +29,7 @@ function getDetails(id) {
             type: "GET",
             contentType: "application/json; charset=utf-8",
             // submit a get request to the restful service mktDetail.
-            url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id,
+            url: "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id,
             dataType: 'jsonp'
         }).then(function (secondResponse) {
             var marketDetails = secondResponse.marketdetails
